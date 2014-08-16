0c181165da2a373026c3ac147a8002aee9e0f0d9
mcsimenc <mcsimenc@users.noreply.github.com>
2014-08-16 11:14:06 -0700

    Update plot_network_stats.R

--- plot_network_stats.R
+++ plot_network_stats.R
@@ -7,16 +7,19 @@
 
 
 require(ggplot2)
-require(reshape)
 require(scales)
 require(RColorBrewer)
+
 raw_data <- read.table("summary.txt",header=T)
 
 network_stats_single_plot <- function (stats) {
 	
 	colors <- brewer.pal(3,"Blues")
 	measure <- names(stats)[2]
-	stat_plot <- ggplot(data=stats,aes_string(x="Year",y=measure)) + geom_line(aes(),colour=colors[3]) + geom_point(aes(),colour=colors[3])+theme(panel.border=element_rect(colour="black",fill=NA),panel.grid.minor=element_blank(),panel.grid.major=element_blank(),panel.background=element_blank(),legend.position="none",axis.text=element_text(size=8)) + xlab("")+ scale_x_continuous(breaks=seq(2000,2014,2))
+	stat_plot <- ggplot(data=stats,aes_string(x="Year",y=measure)) + geom_line(aes(),colour=colors[3]) + 
+		geom_point(aes(),colour=colors[3]) + theme(panel.border=element_rect(colour="black",fill=NA),
+		panel.grid.minor=element_blank(),panel.grid.major=element_blank(),panel.background=element_blank(),
+		legend.position="none",axis.text=element_text(size=8)) + xlab("") + scale_x_continuous(breaks=seq(2000,2014,2))
 
 	return(stat_plot)
 
