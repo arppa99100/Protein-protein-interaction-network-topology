01578cde6bf4a756c9d7580132a9ff7603ecccd3
mcsimenc <mcsimenc@users.noreply.github.com>
2014-08-16 00:20:51 -0700

    Update create_ntwk_get_stats_3fold_tab2.py

--- create_ntwk_get_stats_3fold_tab2.py
+++ create_ntwk_get_stats_3fold_tab2.py
@@ -6,9 +6,10 @@ import os
 def num_pubs(data_dir):
 
 	'''
-	Parses tab2 files in a data_dirtory returning a
+	Parses tab2 files in data_diretory returning a
 	dictionary with an interaction pair mapping to
-	a set of reference publications.
+	a set of reference publications. For use in
+	determining 3-fold validation.
 	'''
 
 	os.chdir(data_dir)
