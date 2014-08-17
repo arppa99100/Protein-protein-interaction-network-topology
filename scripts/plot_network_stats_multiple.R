ace0c1de2e468e00cae1bf8279ade04069016d74
System Administrator <root@simencma.local>
2014-08-17 14:11:18 -0700

    update

--- scripts/plot_network_stats_multiple.R
+++ scripts/plot_network_stats_multiple.R
@@ -2,6 +2,7 @@ require(ggplot2)
 require(reshape2)
 require(scales)
 require(wq)
+require(gridExtra)
 
 setwd('../output')
 
@@ -9,7 +10,7 @@ network_stats_single_plot <- function (stats) {
 	
 	measure <- names(stats)[2]
 	stat_plot <- ggplot(data=stats,aes(x=Year,y=value,colour = variable)) + geom_line() + 
-	geom_point(aes(),colour="#3182BD")+theme(panel.border=element_rect(colour="black",fill=NA),panel.grid.minor=element_blank(),
+	geom_point(aes())+theme(panel.border=element_rect(colour="black",fill=NA),panel.grid.minor=element_blank(),
 	panel.grid.major=element_blank(),panel.background=element_blank(),legend.position="none",
 	axis.text=element_text(size=8)) + xlab("")+ scale_x_continuous(breaks=seq(2000,2014,2))
 
@@ -45,7 +46,7 @@ normalize_df <- function (data_file,cols=c(1,len(data_file))) {
 	 return (data_file)
 	 }
 
-	
+
 #------------------------------------------------------------------------------------------
 
 
@@ -62,20 +63,20 @@ norm_rv_2 <- normalize_df(running_variances_2,c(2,5))
 
 
 interactions <- cbind(topology_measures[1:2],topology_2[2])
-names(interactions) <- c("Year","1","2")
+names(interactions) <- c("Year","Binary","Co-complex")
 transitivity <- cbind(topology_measures[1], topology_measures[3],topology_2[3])
-names(transitivity) <- c("Year","1","2")
+names(transitivity) <- c("Year","Binary","Co-complex")
 cpl <- cbind(topology_measures[1], topology_measures[4],topology_2[4])
-names(cpl) <- c("Year","1","2")
+names(cpl) <- c("Year","Binary","Co-complex")
 assortativity <- cbind(topology_measures[1], topology_measures[5],topology_2[5])
-names(assortativity) <- c("Year","1","2")
+names(assortativity) <- c("Year","Binary","Co-complex")
 
 interactions <- melt(interactions, id="Year")
 transitivity <- melt(transitivity, id="Year")
 cpl <- melt(cpl, id="Year")
 assortativity <- melt(assortativity, id="Year")
 
-int_plot <- network_stats_single_plot(interactions) + ylab("Interactions")
+int_plot <- network_stats_single_plot(interactions) + ylab("Interactions") + theme(legend.position="top",legend.title=element_blank())
 trans_plot <- network_stats_single_plot(transitivity) + ylab("Transitivity")
 cpl_plot <- network_stats_single_plot(cpl) + ylab("CPL")
 assort_plot <- network_stats_single_plot(assortativity) + ylab("Assortativity")
@@ -85,13 +86,13 @@ assort_plot <- network_stats_single_plot(assortativity) + ylab("Assortativity")
 
 
 interactions_var <- cbind(norm_rv[1:2],norm_rv_2[2])
-names(interactions_var) <- c("Year","1","2")
+names(interactions_var) <- c("Year","Binary","Co-complex")
 transitivity_var <- cbind(norm_rv[1], norm_rv[3],norm_rv_2[3])
-names(transitivity_var) <- c("Year","1","2")
+names(transitivity_var) <- c("Year","Binary","Co-complex")
 cpl_var <- cbind(norm_rv[1], norm_rv[4],norm_rv_2[4])
-names(cpl_var) <- c("Year","1","2")
+names(cpl_var) <- c("Year","Binary","Co-complex")
 assortativity_var <- cbind(norm_rv[1], norm_rv[5],norm_rv_2[5])
-names(assortativity_var) <- c("Year","1","2")
+names(assortativity_var) <- c("Year","Binary","Co-complex")
 
 interactions_var <- melt(interactions_var, id="Year")
 transitivity_var <- melt(transitivity_var, id="Year")
@@ -108,7 +109,6 @@ assort_plot_var <- running_variance_plot(assortativity_var)
 
 
 # multiplot(int_plot, int_plot_var, trans_plot,  trans_plot_var, cpl_plot, cpl_plot_var, assort_plot,assort_plot_var,cols=2)
-
 layOut(list(int_plot,1:3,1:2),
 		list(int_plot_var,4:5,1:2),
 		list(cpl_plot,1:3,3:4),
@@ -116,7 +116,7 @@ layOut(list(int_plot,1:3,1:2),
 		list(trans_plot,6:8,1:2),
 		list(trans_plot_var,9:10,1:2),
 		list(assort_plot,6:8,3:4),
-		list(assort_plot,9:10,3:4))
+		list(assort_plot_var,9:10,3:4))
 
 
 # g_legend<-function(p){
@@ -125,13 +125,10 @@ layOut(list(int_plot,1:3,1:2),
 # legend <- tmp$grobs[[leg]]
 # return(legend)}
 
-
-
 # legend_plot <- ggplot(data=interactions,aes(x=Year,y=value,colour = variable)) + geom_line()
-# leg_plot <- ggplot(data=interactions,
+# leg_plot <- ggplot(data=interaction)
 # legend <- g_legend(legend_plot)
 
-# grid.arrange(arrangeGrob(int_plot,trans_plot,cpl_plot,assort_plot,int_plot_var,trans_plot_var,cpl_plot_var,assort_plot_var,ncol=2),legend)
 
+# grid.arrange(arrangeGrob(arrangeGrob(int_plot,trans_plot,cpl_plot,assort_plot,int_plot_var,trans_plot_var,cpl_plot_var,assort_plot_var),legend,ncol=2),heights=1:8)
 
-# grid.arrange(arrangeGrob(arrangeGrob(int_plot,trans_plot,cpl_plot,assort_plot,int_plot_var,trans_plot_var,cpl_plot_var,assort_plot_var),legend))
